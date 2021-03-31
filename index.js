156ac0c114d349e7b27e504590c011953d6c8e33
anik055 <59714409+anik055@users.noreply.github.com>
2021-03-31 07:44:03 +0600

    first

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-res.send('Hello World!!!!');
+res.send('Hello World!');
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rzm4j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
